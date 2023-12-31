c391ac69a5cbc9dce1a9399c5098d368bcedf8a0
Cristian Glz <cristian.gonzalezf-ext@ab-inbev.com>
2023-12-30 18:07:22 -0600

    Render del todo

---
+++ src/todos/use-cases/create-todo-html.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {Todo} todo 
+ */
+export const createTodoHTML = (todo) => {
+
+    if(!todo) throw new Error ('A TODO object is required');
+
+    const html = `<h1>${todo.description}</h1>`;
+    const liElement = document.createElement('li');
+    liElement.innerHTML = html;
+
+    return liElement;
+
+}
---
+++ src/todos/use-cases/render-todos.js
@@ -0,0 +1,16 @@
+import { createTodoHTML } from "./create-todo-html";
+
+/**
+ * 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = ( elementId, todos = [] ) => {
+
+    // TODO: referencia
+    const element = document.querySelector(elementId);
+    todos.forEach(todo => {
+        element.append(createTodoHTML(todo));
+    })
+    
+}
